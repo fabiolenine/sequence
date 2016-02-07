be3f434db4ff5fdabd2fce5e1273b6ac93e53883
Fabio Lenine <fabiolenine@gmail.com>
2016-02-06 22:36:03 -0300

    Teste
    Alterações de configurações para testes.

--- sequence.js
+++ sequence.js
@@ -8,12 +8,12 @@ var app     = express();
 
 app.use(vhost('sequence.lenines.com',app));
 
-http.createServer(app).listen(80);
+app.listen(80);
 //https.createServer(options, app).listen(443);
 
 app.use(express.static('public'));
 app.set('view engine','ejs');
-app.set('views','views/lenines');
+app.set('views','views');
 
 app.get('/', function(req, res){
     res.send('Teste');
